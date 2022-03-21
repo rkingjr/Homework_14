cc847761981a10e8ae3728e9638d6340fef63c0a
Randy King Jr <rkingjr@outlook.com>
2022-03-20 23:56:39 -0500

    update users api routes

---
+++ controllers/api/users-routes.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+const {Users} = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const newUser = await Users.create({
+            username: req.body.username,
+            password: req.body.password
+        });
+
+        req.session.save(() => {
+            req.session.userId = newUser.id;
+            req.session.username = newUser.username;
+            req.session.loggedIn = true;
+            res.json(newUser);
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const user = await Users.findOne({
+        where: {
+            username: req.body.username,
+        },
+    });
+    if (!user) {
+        res.status(400).json({ message: 'No user account found!' });
+        return;
+    }
+
+    const validPassword = user.checkPassword(req.body.password);
+
+    if (!validPassword) {
+        res.status(400).json({ message: 'No user account found!' });
+        return;
+    }
+
+    req.session.save(() => {
+        req.session.userId = Users.id;
+        req.session.username = Users.username;
+        req.session.loggedIn = true;
+        res.json({ user, message: 'You are now logged in!' });
+    });
+} catch (err) {
+    res.status(400).json({ message: 'No user account found!' });
+}
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+module.exports = router;
