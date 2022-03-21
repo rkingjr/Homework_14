f36809a31bc8f9ae0a3a7e3353d07cda3673935d
Randy King Jr <rkingjr@outlook.com>
2022-03-20 22:33:27 -0500

    finish server

--- server.js
+++ server.js
@@ -11,4 +11,30 @@ const sequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const hbs = handlebars.create({helpers});
 
+const sessions = {
+    secret: 'Tech Blog Secreat',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new sequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sessions));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}.`);
+    sequelize.sync({ force: false });
+});
