4b06ed023d9647c7b81cfcd2a5b73f943f26e5b3
Randy King Jr <rkingjr@outlook.com>
2022-03-20 23:48:16 -0500

    update posts comments

---
+++ controllers/api/posts-routes.js
@@ -0,0 +1,54 @@
+const router = require('express').Router();
+const {Posts} = require('../../models/');
+const withAuth = require('../../utils/auth');
+
+router.post('/', withAuth, async (req, res) => {
+    const body = req.body;
+        console.log(body);
+    try {
+        const newPost = await Posts.create({ ...body, userId: req.session.userId });
+        console.log("Here is the new post: ",  newPost);
+        res.json(newPost);
+            } catch (err) {
+                console.log('IT FAILED!', err);
+        res.status(500).json(err);
+    }
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        console.log('here is the req.body', req.body);
+        const [affectedRows] = await Posts.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+
+        if (affectedRows > 0) {
+            res.status(200).end();
+        } else {
+            res.status(404).end();
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const [affectedRows] = Posts.destroy({
+        where: {
+            id: req.params.id,
+        },
+    });
+    if (affectedRows > 0) {
+        res.status(200).end();
+    } else {
+        res.status(404).end();
+    }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
